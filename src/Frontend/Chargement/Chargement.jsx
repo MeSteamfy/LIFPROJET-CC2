3fb27ffaece7a6bf4abc9358641e5d55f24799c4
MeAllister <anselmeyeffe@gmail.com>
2025-01-21 20:21:41 +0100

    Ajouts de quelque fonctionalités dans Main.jsx et le CSS

---
+++ src/Frontend/Chargement/Chargement.jsx
@@ -0,0 +1,11 @@
+import styles from './Chargement.module.css'; 
+
+const Chargement = () => {
+    return (
+        <div className={styles.chargement}>
+            <div className={styles.spinneur}></div>
+        </div>
+    );
+};
+
+export default Chargement;
--- src/Frontend/Main Component/Main.jsx
+++ src/Frontend/Main Component/Main.jsx
@@ -1,13 +1,37 @@
+import { useRef } from "react";
 import styles from "./Main.module.css"
 
 function Main() {
+    const inputValeurRef = useRef(null);
+    const valRandomPlaceholder = ["Sulfura", "Meloetta", "Tortank", "Marisson", "Spiritomb", "Golemastoc", "Alakazam", "Laggron"];
+
+    function testIcon() {
+        if (inputValeurRef.current && inputValeurRef.current.value.length > 0) {
+            console.log("Envoi dans le backend...");
+        }
+    }
+
     return (
         <div className={styles.mainConteneur}>
-            <h1 className="titre">PoketchAPI</h1>
-
             <p className={styles.description}>
-                Le site qui vous donne les meilleurs informations sur les cartes Pokémon!
+                Prédiction des Prix des Cartes à Collectionner <span className={styles.span}>Pokémon TCG!</span>
             </p>
+            <div className={styles.bas}>
+                <div className={styles.gauche}>
+                    <h1 className="titre">PoketchAPI</h1>
+                    <div className={styles.inputConteneur}>
+                        <input type="text" ref={inputValeurRef} className={styles.input} placeholder={`${valRandomPlaceholder[Math.floor(Math.random()*valRandomPlaceholder.length)]}...`} />
+                        <i onClick={testIcon} className={`fa-solid fa-magnifying-glass ${styles.icon}`} />
+                    </div>
+                    <p className={styles.info}>
+                        Ce site utilise l'API <span className={styles.span}>[indiquer ici le nom de l'api qu'on utilisera]</span> pour récupérer les données.
+                    </p>
+                </div>
+                
+                <img src={`/${Math.ceil(Math.random()*2)}.jpg`} className={styles.image} />
+            </div>
+            
+
         </div>
     )
 }
