de8146845c3a8c57008e6ef4391bba76d1b6f99d
MeAllister <herve.yeffe@etu.univ-lyon1.fr>
2025-01-22 15:21:27 +0100

    Update des styles du footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Frontend/Header Component/Header"
 import JSONSection from "./Frontend/JSON Component/JSONSection"
 import Transition from "./Frontend/Transition/Transition"
 import Main from "./Frontend/Main Component/Main"
+import Footer from "./Frontend/Footer Component/Footer"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Transition styles="mainJson" />
       <JSONSection />
       <Transition styles="jsonFooter" />
+      <Footer />
     </>
   )
 }
--- src/Frontend/Footer Component/Footer.jsx
+++ src/Frontend/Footer Component/Footer.jsx
@@ -3,9 +3,8 @@ import styles from './Footer.module.css'
 function Footer() {
     return(
         <footer className={styles.footerConteneur}>
-            <h1 className="jsonFooter">Ceci est le footer</h1>
+            <h1>Ceci est le footer</h1>
         </footer>
-      
     )
 }
 
