e40adaf2c42351e6de3cba9c7767462c39d51cb1
Faik Jebari <faik.jebari@etu.univ-lyon1.fr>
2025-01-22 15:11:23 +0100

    initialisation du footer

---
+++ src/Frontend/Footer Component/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer className={styles.footerConteneur}>
+            <h1 className="jsonFooter">Ceci est le footer</h1>
+        </footer>
+      
+    )
+}
+
+export default Footer;
