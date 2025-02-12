34c723be81d48d1d8f07ca01baa865d9bad4f2a7
MeAllister <herve.yeffe@etu.univ-lyon1.fr>
2025-02-12 10:42:57 +0100

    update app.py

--- src/Backend/app.py
+++ src/Backend/app.py
@@ -42,21 +42,8 @@ def getSetById(id):
             if card: 
                 print(card.total)
                 card_dict = {
-<<<<<<< HEAD
-                    'id': card.id, 
-                    'name': card.name,
-                    'rarity': card.rarity,
-                    'set': card.set,
-                    'images': card.images,
-                    
-                    
-                    
-                    
-                    
-=======
                     'id': card.id,
-                    'images': card.images
->>>>>>> 00089f127c1a1f104678e543636125131d3cc4e0
+                    'images': card.images,
                 }
                 tabCartes.append(card_dict) 
 
