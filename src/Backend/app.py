a11011bf493bcfa861849bbb6e5221ac1ac091c2
Blurheet <tonyx69120@gmail.com>
2025-02-17 10:35:42 +0100

    liaison entre github et modele

--- src/Backend/app.py
+++ src/Backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import tensorflow as tf
 import pokemontcgsdk
 from pokemontcgsdk import Card
 from pokemontcgsdk import Set
--- src/Backend/model.py
+++ src/Backend/model.py
@@ -3,3 +3,15 @@ import numpy as np
 import sklearn 
 from sklearn import linear_model
 import pickle
+import app
+import requests
+
+
+url = "https://raw.githubusercontent.com/tcgdex/price-history/master/en/base1/1.tcgplayer.json"  
+response = requests.get(url)    
+data = response.json()
+print(data)
+#data = data[]
+#predict = "avg"
+
+#x = np.array(data.drop(predict))
